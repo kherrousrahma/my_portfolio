e5f4010a7013e4a7a41d4e37a9f48bb21878cc92
KHERROUS Rahma <kherrousrahma13@gmail.com>
2025-09-07 23:34:20 +0100

    Hero updates

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Hero.css'
+import HeroImage from '../../assets/Hero.png'
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex">
+        <div className="hero-content">
+            <span className="sub-text">Hello, there!</span>
+            <h1>
+                <span className='green-text'>I'm Rahma,</span>
+                Front-End Developer & UX/UI Designer
+            </h1>
+            <p>
+                Passionate about developing high-performance web and mobile applications, proficient in modern technologies such as React.js and Vue.js for front-end, Flutter for mobile development.
+            </p>
+            <div>
+                <a href="#" className="btn">View My works</a>
+                <a href="#" className="btn border-btn">View My works</a>
+            </div>
+        </div>
+        <div className="hero-image">
+          <img src={HeroImage} alt="Hero" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
