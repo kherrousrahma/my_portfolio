a32be5084fc8231ab8b31ea30270e53165592fc2
KHERROUS Rahma <kherrousrahma13@gmail.com>
2025-09-11 23:14:20 +0100

    updates Hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import './Hero.css'
 import HeroImage from '../../assets/Hero.png'
+import CV from '../../assets/CV.pdf'
 
 const Hero = () => {
   return (
     <section>
-      <div className="hero-section flex">
+      <div className="hero-section flex wrapper gap-4">
         <div className="hero-content">
             <span className="sub-text">Hello, there!</span>
             <h1>
                 <span className='green-text'>I'm Rahma,</span>
-                Front-End Developer & UX/UI Designer
+                <br />Front-End Developer & UX/UI Designer.
             </h1>
             <p className='para'>
                 Passionate about developing high-performance web and mobile applications, proficient in modern technologies such as React.js and Vue.js for front-end, Flutter for mobile development.
             </p>
-            <div>
+            <div className="flex gap-2">
                 <a href="#" className="btn">View My works</a>
-                <a href="#" className="btn border-btn">View My works</a>
+                <a href={CV} download="My CV" className="btn border-btn">Download CV</a>
             </div>
         </div>
         <div className="hero-image">
